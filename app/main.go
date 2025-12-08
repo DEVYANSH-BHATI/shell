4e1e24bb8c979e402b5fa8784e28ac2bace50481
devyansh <devyansh.bhati333@gmail.com>
2025-12-08 23:45:53 +0530

    cmt2

--- app/main.go
+++ app/main.go
@@ -10,4 +10,9 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+	var cmd string
+	fmt.Scanln(&cmd)
+	fmt.Print(cmd)
+	fmt.Println(": command not found")
+
 }
