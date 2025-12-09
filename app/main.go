0d6398a9ef08292f68f1553718fa425a93106d05
devyansh <devyansh.bhati333@gmail.com>
2025-12-09 10:21:02 +0530

    commit

--- app/main.go
+++ app/main.go
@@ -9,10 +9,14 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
-	var cmd string
-	fmt.Scanln(&cmd)
-	fmt.Print(cmd)
-	fmt.Println(": command not found")
+	for {
+
+		fmt.Print("$ ")
+		var cmd string
+		fmt.Scanln(&cmd)
+		fmt.Print(cmd)
+		fmt.Println(": command not found")
+
+	}
 
 }
