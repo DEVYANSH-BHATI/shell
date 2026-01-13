d33b04790963faa2d2863f20a675d3a7e12d0faf
devyansh <devyansh.bhati333@gmail.com>
2026-01-13 10:56:39 +0530

    doublequotes

--- app/main.go
+++ app/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 	"slices"
 	"strings"
 )
@@ -120,4 +122,133 @@ func typee(token string) {
 
 	}
 
+}
+
+// package main
+
+// import "strings"
+
+func tokenize(cmd string) []string {
+	//tokenize without strings.fields
+	tokens := []string{}
+	inSingleQuote := false
+	inDoubleQuote := false
+	var currentToken strings.Builder
+	for _, char := range cmd {
+		if char == '"' {
+			inDoubleQuote = !inDoubleQuote
+			continue
+		}
+
+		if inDoubleQuote {
+			currentToken.WriteRune(char)
+			continue
+		}
+
+		if char == '\'' {
+			inSingleQuote = !inSingleQuote
+			continue
+		}
+
+		if char == ' ' || char == '\t' {
+			if inSingleQuote {
+				currentToken.WriteRune(char)
+				continue
+			} else {
+				if currentToken.Len() > 0 {
+					tokens = append(tokens, currentToken.String())
+					currentToken.Reset()
+				}
+				continue
+			}
+		}
+
+		currentToken.WriteRune(char)
+	}
+	if currentToken.Len() > 0 {
+		tokens = append(tokens, currentToken.String())
+		currentToken.Reset()
+	}
+	return tokens
+}
+
+// func tokenize(cmd string) []string {
+// 	var tokens []string
+// 	var current strings.Builder
+// 	inSingleQuote := false
+
+// 	for _, ch := range cmd {
+// 		switch ch {
+// 		case '\'':
+// 			inSingleQuote = !inSingleQuote
+
+// 		case ' ', '\t':
+// 			if inSingleQuote {
+// 				current.WriteRune(ch)
+// 			} else if current.Len() > 0 {
+// 				tokens = append(tokens, current.String())
+// 				current.Reset()
+// 			}
+
+// 		default:
+// 			current.WriteRune(ch)
+// 		}
+// 	}
+
+// 	if current.Len() > 0 {
+// 		tokens = append(tokens, current.String())
+// 	}
+
+// 	return tokens
+// }
+
+func findExecutable(token string) (bool, string) {
+	cur_os := runtime.GOOS
+	pathEnv := os.Getenv("PATH")
+	if pathEnv == "" {
+		fmt.Println("PATH environment variable is not set.")
+		return false, "emptypath"
+	}
+	if cur_os == "windows" {
+		directories := strings.Split(pathEnv, ";")
+
+		// // break pathenv into different directories
+		// iterate on each dir and check if tokenpath exists in that dir
+		// check if its regular file and is executable
+		for _, dir := range directories {
+
+			// fmt.Println(dir)
+			tokenpath := filepath.Join(dir + "/" + token)
+			info, err := os.Stat(tokenpath)
+			if err == nil {
+				if info.Mode().IsRegular() {
+					if info.Mode()&0111 != 0 {
+						return true, tokenpath
+					}
+				}
+			}
+		}
+		return false, ""
+
+		// break
+
+		// }
+	} else {
+		directories := strings.Split(pathEnv, ":")
+		for _, dir := range directories {
+
+			tokenpath := filepath.Join(dir + "/" + token)
+			info, err := os.Stat(tokenpath)
+			if err == nil {
+				if info.Mode().IsRegular() {
+					if info.Mode()&0111 != 0 {
+						return true, tokenpath
+					}
+				}
+			}
+		}
+
+		// break
+		return false, ""
+	}
 }
