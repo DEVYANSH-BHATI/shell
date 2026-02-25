03062ca675ff18610bc9c759a55c32fc7dc7d204
devyansh <devyansh.bhati333@gmail.com>
2026-02-25 14:53:39 +0530

    re structuring the code for parser

--- app/main.go
+++ app/main.go
@@ -16,6 +16,14 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+type Command struct {
+	Name    string
+	Args    []string
+	Stdout  io.Writer
+	Stderr  io.Writer
+	Cleanup func()
+}
+
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	scanner := bufio.NewScanner(os.Stdin)
@@ -35,104 +43,54 @@ func main() {
 		// 	fmt.Println(token)
 		// }
 
-		// Redirection logic :
-		// if command has 1> or > means out needs redirection
-		// so we will define new io.writer (output) by default same as os.stdout that is out cli
-		// but if > is detected we take the filename from command and open the file for edit 2
-		var output io.Writer = os.Stdout
-		var erroutput io.Writer = os.Stderr
-		var err_openfile *os.File
-		var openfile *os.File
-		tokens_copy := tokens
-		var filepath_redirection string
-		redirectionexists := slices.Index(tokens, ">")
-		stderr_redirection_exists := slices.Index(tokens, "2>")
-		var stderr_filepath_redirection string
-		if stderr_redirection_exists != -1 {
-			tokens_copy = tokens[:stderr_redirection_exists]
-			stderr_filepath_redirection = tokens[stderr_redirection_exists+1]
-			err_openfile, err := os.OpenFile(stderr_filepath_redirection,
-				os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
-				0644)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			erroutput = err_openfile
+		command := parsecommand(tokens)
 
-		}
-		if redirectionexists == -1 {
-			redirectionexists = slices.Index(tokens, "1>")
-		}
-		if redirectionexists != -1 {
-			tokens_copy = tokens[:redirectionexists]
-			if redirectionexists+1 >= len(tokens) {
-				fmt.Println("syntax error")
-			}
-			filepath_redirection = tokens[redirectionexists+1]
-			openfile, err := os.OpenFile(filepath_redirection,
-				os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
-				0644)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			output = openfile
-
-		}
-
-		if len(tokens_copy) == 0 {
+		if len(command.Args) == 0 {
 			continue
 		}
-		switch strings.ToLower(tokens_copy[0]) {
+		switch strings.ToLower(command.Args[0]) {
 		case "echo":
-			echo(tokens_copy, output)
+			echo(command.Args, command.Stdout)
 
 		case "type":
-			typee(strings.ToLower(tokens_copy[1]), output, erroutput)
+			typee(strings.ToLower(command.Args[1]), command.Stdout, command.Stderr)
 
 		case "pwd":
 			pwd, _ := os.Getwd()
-			fmt.Fprintln(output, pwd)
+			fmt.Fprintln(command.Stdout, pwd)
 
 		case "cd":
 			var err error
-			if tokens_copy[1] == "~" {
+			if command.Args[1] == "~" {
 				homepath := os.Getenv("HOME")
 				err = os.Chdir(homepath)
 			} else {
-				err = os.Chdir(tokens_copy[1])
+				err = os.Chdir(command.Args[1])
 			}
 			if err != nil {
-				fmt.Fprintln(output, "cd: "+tokens_copy[1]+": No such file or directory")
+				fmt.Fprintln(command.Stdout, "cd: "+command.Args[1]+": No such file or directory")
 			}
 
 		case "exit":
 
 		default:
-			foundExecutable, pathOfExecutable := findExecutable(tokens_copy[0])
+			foundExecutable, pathOfExecutable := findExecutable(command.Args[0])
 			if foundExecutable {
-				args := tokens_copy[1:]
+				args := command.Args[1:]
 				cmd := exec.Command(pathOfExecutable, args...)
 				cmd.Stdin = os.Stdin
-				cmd.Stdout = output
-				cmd.Stderr = erroutput
-				cmd.Args[0] = tokens_copy[0]
+				cmd.Stdout = command.Stdout
+				cmd.Stderr = command.Stderr
+				cmd.Args[0] = command.Args[0]
 				cmd.Run()
 
 			} else {
-				fmt.Print(tokens_copy[0])
+				fmt.Print(command.Args[0])
 				fmt.Println(": command not found")
 			}
 
 			// break
 
-		}
-		if openfile != nil {
-			openfile.Close()
-		}
-		if err_openfile != nil {
-			err_openfile.Close()
 		}
 
 		if strings.ToLower(cmd) == "exit" {
@@ -295,3 +253,114 @@ func tokenize(cmd string) []string {
 	}
 	return tokens
 }
+
+func parsecommand(tokens []string) *Command {
+	// Redirection logic :
+	// if command has 1> or > means out needs redirection
+	// so we will define new io.writer (output) by default same as os.stdout that is out cli
+	// but if > is detected we take the filename from command and open the file for edit 2
+
+	cmd := &Command{
+		Stdout:  os.Stdout,
+		Stderr:  os.Stderr,
+		Cleanup: func() {},
+	}
+
+	for pos := 0; pos < len(tokens); pos++ {
+		// for pos, cur_token := range tokens {
+		cur_token := tokens[pos]
+		if cur_token == "1>" || cur_token == ">" {
+			if pos+1 >= len(tokens) {
+				fmt.Println("syntax error")
+				return cmd
+			}
+			var openfile *os.File
+			openfile, err := os.OpenFile(tokens[pos+1],
+				os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+				0644)
+			if err != nil {
+				fmt.Println(err)
+				return cmd
+			}
+			cmd.Stdout = openfile
+			output_file_cleanup := cmd.Cleanup
+			cmd.Cleanup = func() {
+				output_file_cleanup()
+				openfile.Close()
+			}
+			pos++
+			continue
+		}
+
+		if cur_token == "2>" {
+			if pos+1 >= len(tokens) {
+				fmt.Println("syntax error")
+				return cmd
+			}
+			var err_openfile *os.File
+			err_openfile, err := os.OpenFile(tokens[pos+1],
+				os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+				0644)
+			if err != nil {
+				fmt.Println(err)
+				return cmd
+			}
+			cmd.Stderr = err_openfile
+			err_file_cleanup := cmd.Cleanup
+			cmd.Cleanup = func() {
+				err_file_cleanup()
+				err_openfile.Close()
+			}
+			pos++
+			continue
+		}
+		cmd.Args = append(cmd.Args, cur_token)
+	}
+	if len(cmd.Args) > 0 {
+		cmd.Name = cmd.Args[0]
+	}
+
+	return cmd
+}
+
+// // var output io.Writer = os.Stdout
+// // var erroutput io.Writer = os.Stderr
+// // var err_openfile *os.File
+// // var openfile *os.File
+// cmd_object.Args := tokens
+// var filepath_redirection string
+// // redirectionexists := slices.Index(tokens, ">")
+// stderr_redirection_exists := slices.Index(tokens, "2>")
+// var stderr_filepath_redirection string
+// if stderr_redirection_exists != -1 {
+// 	cmd_object.Args = tokens[:stderr_redirection_exists]
+// 	stderr_filepath_redirection = tokens[stderr_redirection_exists+1]
+// 	err_openfile, err := os.OpenFile(stderr_filepath_redirection,
+// 		os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+// 		0644)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		continue
+// 	}
+// 	erroutput = err_openfile
+
+// }
+// if redirectionexists == -1 {
+// 	redirectionexists = slices.Index(tokens, "1>")
+// }
+// if redirectionexists != -1 {
+// 	cmd_object.Args = tokens[:redirectionexists]
+// 	if redirectionexists+1 >= len(tokens) {
+// 		fmt.Println("syntax error")
+// 	}
+// 	filepath_redirection = tokens[redirectionexists+1]
+// 	openfile, err := os.OpenFile(filepath_redirection,
+// 		os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+// 		0644)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		continue
+// 	}
+// 	output = openfile
+
+// }
